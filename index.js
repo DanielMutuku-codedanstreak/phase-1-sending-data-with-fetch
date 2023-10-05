10b0876b430dc3be7e5775d596a34383be4f9572
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-05 09:42:14 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const base_URL = 'http://localhost:3000'
+function submitData(username,useremail){
+    return fetch(`${base_URL}/users`, {
+        method:'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name : username,
+            email : useremail
+        }),
+    }).then(res => res.json())
+    .then(data => {
+        //upon success
+        document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+        //upon error
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    })
+}
+
+//call the function
+let username = 'Daniel Mutuku'
+let useremail = 'mutukudaniel79@gmail.com'
+submitData(username, useremail);
